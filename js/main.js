35aaceb5e0257c817ed9f34685be672ada88e007
YeiJ <63900106+YeiJ@users.noreply.github.com>
2021-12-21 16:47:47 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+function chm(){
+    window.open ('http://bbs.鸽子.top:1000/','讨论区', 'height=800, width=1400, top=0,left=0, toolbar=no, menubar=no, scrollbars=no, resizable=yes ,location=no, status=no') 
+}
+
+function wiki(){
+    window.open ('http://wiki.鸽子.top:1000/','Wiki', 'height=800, width=1400, top=0,left=0, toolbar=no, menubar=no, scrollbars=no, resizable=yes ,location=no, status=no') 
+}
